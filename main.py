b70d736632ab2b15c7432f113f1a61df390d3a1c
MrEricB <eric.biersner@gmail.com>
2019-03-25 19:42:28 -0700

    add user input to move object

--- main.py
+++ main.py
@@ -13,30 +13,47 @@ gameDisplay = pygame.display.set_mode((800, 600)) # the canvas that is displayed
 
 # updates entire pygame surface at once
 # pygame.display.flip()
-
-
 pygame.display.set_caption('Slither')
 
 # only updates the part of the surface you give it. If no params then updates whole surface
 pygame.display.update()
 
+# simple game loop
 gameExit = False
 
+# first block in the snake, ie head of the snake
+lead_x = 300
+lead_y = 300
+lead_x_change = 0
+
+clock = pygame.time.Clock() # return a pygame clock obj for FPS
 
 while not gameExit:
     for event in pygame.event.get():
-        # print(event)
         if event.type == pygame.QUIT:
             gameExit = True
 
+        if event.type == pygame.KEYDOWN:
+            # make sure was an arrow and then witch arrow
+            if event.key == pygame.K_LEFT:
+                lead_x_change = -10
+            if event.key == pygame.K_RIGHT:
+                lead_x_change = 10
+        # allows use to keep moving while holding key down, stops when key is up
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                lead_x_change = 0
+
+
+    lead_x += lead_x_change
+
     gameDisplay.fill(white)
-    pygame.draw.rect(gameDisplay, black, [400,300,10,10])
+    pygame.draw.rect(gameDisplay, black, [lead_x,lead_y,10,10])
     # better way to draw rec but more complexe, since fill can be graphics accelerated
-    gameDisplay.fill(red, rect=[200,200,50,50])
-
-
+    # gameDisplay.fill(red, rect=[200,200,50,50])
     pygame.display.update()
 
+    clock.tick(15) # seting FPS here
 
 # to exit pygame
 pygame.quit() # uninitilzes pygame
